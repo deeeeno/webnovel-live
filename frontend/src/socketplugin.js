d77b50ce43a808a0b8e8475717953f06f3117628
dinohee <kkrs0512@naver.com>
2018-05-15 01:56:16 +0900

    plus comment and understandable sheet

--- frontend/src/socketplugin.js
+++ frontend/src/socketplugin.js
@@ -1,6 +1,8 @@
+//server와 통신이 되는 역할을 하는 javascript
+//Plugin형태로 import해서 사용
 export default function createSocketPlugin (socket) {
   return store => {
-    socket.on("serverHello", (data) => {
+    socket.on("serverHello", (data) => { // serverHello가 오면 store의 data들을 최신화 해줘
       console.log(data);
       store.commit("serverMessageChange", data.msg);
       store.commit("countChange", data.count);
--- frontend/src/store.js
+++ frontend/src/store.js
@@ -4,31 +4,32 @@ import io from "socket.io-client";
 import createSocketPlugin from './socketplugin';
 
 Vue.use(Vuex)
+//Vuex : Vue.js application에 대한 상태 관리 패턴 + 라이브러리.
+//application의 모든 컴포넌트에 대한 중앙 집중식 저장소 역할.
 
-const socket = io("http://localhost:3000")
+const socket = io("http://localhost:3000") // for server communication
+const plugin = createSocketPlugin(socket); // same as
 
-
-const plugin = createSocketPlugin(socket);
-
+//저장소 생성 & 외부에서 사용가능하도록.
 export default new Vuex.Store({
-  state: {
+  state: { // state 객체 선언 & 초기화
     count: 0,
     serverMessage: "Nothing yet",
   },
   mutations: {
-    increment (state) {
+    increment (state) { //increment handler에 들어오면
       state.count++
     },
-    serverMessageChange (state, msg) {
+    serverMessageChange (state, msg) { //serverMessageChange handler에 들어오면
       state.serverMessage = msg
     },
-    countChange (state, x) {
+    countChange (state, x) { //countChange handler에 들어오면
       state.count = x;
     }
   },
   actions: {
     //need some more knowledge
-    sendMessage (context) {
+    sendMessage (context) {// HelloWorld.vue파일에서 inc버튼이 눌리면 server에게 hello event handler로 쏨
       socket.emit("hello", { greetings: "hello", count: this.state.count });
     }
   },
