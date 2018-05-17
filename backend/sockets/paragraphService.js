7085cdc83b987ea03db05ada309595ddaa4904ff
JIYUN <dlwldbs1026@gmail.com>
2018-05-17 23:12:01 +0900

    calculating index error fix

--- backend/sockets/paragraphService.js
+++ backend/sockets/paragraphService.js
@@ -1,14 +1,36 @@
 module.exports = function (socket) {
 
-  const paragraphs = new Map([]);
+  const paragraphs = new Map([
+    [0, {
+      content: 'Hello World!',
+      lock: false,
+      owner: ''
+    }],
+    [1, {
+      content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+      lock: false,
+      owner: ''
+    }],
+    [2, {
+      content: 'Nam dui velit, tempor a magna vitae, vulputate aliquam lacus.',
+      lock: false,
+      owner: ''
+    }]
+  ]);
+  
+  const MIN_INDEX = -99999;
 
   socket.on("paragraphAdd", (nextParaIndex) => {
     //create paragraph with calculated Index with claculateIndex(nextParaIndex)
     
     let index = calculateIndex(nextParaIndex);
-    let content = '';
+    let data = {
+      content: '',
+      lock: false,
+      owner: 'defaultUser'
+    };
     
-    paragraphs.set(index, content);
+    paragraphs.set(index, data);
     
     console.log(paragraphs);
     // socket.emit
@@ -34,7 +56,7 @@ module.exports = function (socket) {
   });
   
   function findPrevParaIndex(nextParaIndex) {
-    let prevIndex = 0;
+    let prevIndex = MIN_INDEX;
     paragraphs.forEach(function (value, key, map) {
       let temp = key;
       if (temp < nextParaIndex && temp > prevIndex) {
@@ -49,8 +71,8 @@ module.exports = function (socket) {
     let newIndex;
     let prevIndex = findPrevParaIndex(nextParaIndex);
     
-    if (prevIndex === 0) {
-      newIndex = 0;
+    if (prevIndex === MIN_INDEX) {
+      newIndex = nextParaIndex - 1;
     } else if (!nextParaIndex) {
       newIndex = prevIndex + 1;
     } else {
